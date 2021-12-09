c4637f3ac44cca1c600f989ce6779a4cfc97ffb2
Milind-T0914 <milind.mohapatra@tavant.com>
2021-12-09 17:38:31 +0530

    Add AsDTO extension method on User model

---
+++ Congo/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using Congo.DTOs;
+using Congo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Congo
+{
+    public static class Extensions
+    {
+        public static UserDTO AsDTO(this User User)
+        {
+            return new UserDTO
+            {
+                Id = User.Id,
+                Name = User.Name,
+                Email = User.Email,
+                Password = User.Password,
+                PhoneNumber = User.PhoneNumber,
+                Role = User.Role
+            };
+        }
+    }
+}
