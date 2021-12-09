d5888facf50c107eb0723e389e3bccacbf5dc9fa
Milind-T0914 <milind.mohapatra@tavant.com>
2021-12-09 17:16:29 +0530

    Add LoginUserDTO

---
+++ Congo/DTOs/LoginUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Congo.DTOs
+{
+    public record LoginUserDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; init; }
+        [Required]
+        public string Password { get; init; }
+    }
+}
