731f1e5826a83f6de8deb53cd6b2a3775ac7123b
Milind-T0914 <milind.mohapatra@tavant.com>
2021-12-09 17:14:29 +0530

    Add UpdateUserDTO

---
+++ Congo/DTOs/UpdateUserDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Congo.DTOs
+{
+    public record RegisterUserDTO
+    {
+        [Required]
+        public string Name { get; init; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; init; }
+        [Required]
+        public string Password { get; init; }
+        [Required]
+        public string PhoneNumber { get; init; }
+        [Required]
+        [RegularExpression("^(?i)(buyer|seller)$")]
+        public string Role { get; init; }
+    }
+}
