b33e56440d8fcdbb9ec32aa2dd26171d950ec36f
Milind-T0914 <milind.mohapatra@tavant.com>
2021-12-09 17:14:54 +0530

    Fix class name of updateuserdto

--- Congo/DTOs/UpdateUserDTO.cs
+++ Congo/DTOs/UpdateUserDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Congo.DTOs
 {
-    public record RegisterUserDTO
+    public record UpdateUserDTO
     {
         [Required]
         public string Name { get; init; }
